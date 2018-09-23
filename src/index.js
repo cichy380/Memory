d76de173fefbec75d8595468aa130e1b6a1a9e0b
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2018-09-23 20:25:51 +0200

    Added Bootstrap-4 styles (SASS configuration)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import './scss/index.scss';
+
 new Vue({
     el: '#app',
     data: {
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,73 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js',
-    },
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.js'
+module.exports = function (env, argv) {
+    let productionMode = argv.mode === 'production',
+        plugins = [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html'),
+                filename: 'index.html',
+                inject: 'body', // Inject assets into the given / 'body' = all JS will be placed at the bottom of body elem.
+                minify: {
+                    collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
+                    removeComments: true,
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            }),
+        ];
+
+    productionMode && plugins.push(new OptimizeCssAssetsPlugin());
+
+    return {
+        entry: path.resolve(__dirname, './src/index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'app.js',
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [ {
-                    loader: 'html-loader',
-                }],
-                exclude: [/node_modules/, /src/],
+        resolve: {
+            alias: {
+                'vue': 'vue/dist/vue.js'
             },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: path.resolve(__dirname, './dist/index.html'),
-            inject: 'body', // Inject assets into the given / 'body' = all JS will be placed at the bottom of body elem.
-            minify: {
-                collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
-                removeComments: true,
-            },
-        }),
-    ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: 'html-loader',
+                    }],
+                    exclude: [/node_modules/, /src/],
+                },
+                {
+                    test: /\.(scss)$/,
+                    use: [
+                        // 'style-loader',
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader', // translates CSS into CommonJS modules
+                            options: {
+                                sourceMap: true,
+                            }
+                        },
+                        {
+                            loader: 'sass-loader', // compiles Sass to CSS
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    ],
+                },
+
+            ]
+        },
+        devtool: productionMode ? 'eval' : 'source-map',
+        plugins: plugins,
+        performance: {
+            hints: false,
+        },
+    }
 };
