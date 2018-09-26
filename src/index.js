f5c07e4f93056c717ab1fd5a07d5a7e1b4b8d8d2
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2018-09-26 20:08:19 +0200

    Changed homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
 
 import './scss/index.scss';
+// setInterval(function(){ alert("Hello"); }, 3000);
+setInterval(() => {
+    if (document.getElementsByClassName('card')[0].className === 'card card-theme-red ml-auto') {
+        document.getElementsByClassName('card')[0].className = 'card card-theme-red ml-auto card-flipped';
+    }
+    else {
+        document.getElementsByClassName('card')[0].className = 'card card-theme-red ml-auto';
+    }
+}, 2000);
 
 new Vue({
     el: '#app',
