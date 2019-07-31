2bd8ad03e4a6f9c80d63d61d2b8efe3a48b9c415
cichy380 <marcin.dobroszek@gmail.com>
2019-07-31 09:47:49 +0200

    Added layout for 'game' view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { StartComponent } from './pages/start/start.component'
+import { GameComponent } from './game/game.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/start', pathMatch: 'full' },
   { path: 'start', component: StartComponent },
+  { path: 'game', component: GameComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { StartComponent } from './pages/start/start.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
