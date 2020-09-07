c76bb42832885b7d7dc523c425fd039e3a467082
cichy380 <marcin.dobroszek@gmail.com>
2020-09-07 13:49:27 +0200

    refactor(core): Remove LoggerService

--- src/app/core/services/error-handler.service.ts
+++ src/app/core/services/error-handler.service.ts
@@ -1,18 +1,12 @@
 import { ErrorHandler, Injectable } from '@angular/core'
-import { LoggerService } from './logger.service'
-import { environment } from '../../../environments/environment'
 
 @Injectable()
 export class ErrorHandlerService extends ErrorHandler {
-  constructor(private loggerService: LoggerService) {
+  constructor() {
     super()
   }
 
   public handleError(error: any) {
-    if (environment.production) {
-      this.loggerService.logError(error)
-    }
-
     super.handleError(error)
   }
 }
